859bca946dedfd7e0dde31e07a7ae98109db4a01
Rohit kadam <me.rohitkadam@gmail.com>
2023-05-28 22:45:00 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from "../public/logo512.png";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h3>React app</h3>
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        use: "babel-loader",
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    open: true,
+    liveReload: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
